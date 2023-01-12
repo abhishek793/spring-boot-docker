197c6de4b09f16e89e3e2965880e0e7295b4227f
Abhishek Dubey <abhishek.dubey@nagarro.com>
2023-01-12 13:06:57 +0530

    first commit

---
+++ src/test/java/dubey/abhishek/SpringBootDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package dubey.abhishek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
